f755e15d9d3c7710fc697fac86f45d4968ec84a1
Michael Theriot <michael.lee.theriot@gmail.com>
2026-06-13 17:44:36 -0500

    Add getOrInsert and getOrInsertComputed, clean up

--- WeakValueMap.ts
+++ WeakValueMap.ts
@@ -1,18 +1,15 @@
-type WeakValueMapValue = ConstructorParameters<typeof WeakRef>[0];
+type WeakRefTarget = ConstructorParameters<typeof WeakRef>[0];
 
-class WeakValueMap<TKey, TValue extends WeakValueMapValue> implements Map<TKey, TValue> {
+class WeakValueMap<TKey, TValue extends WeakRefTarget> implements Map<TKey, TValue> {
     #map: Map<TKey, WeakRef<TValue>>;
     #registry: FinalizationRegistry<TKey>;
 
     #unregister(key: TKey): void {
         const value = this.#map.get(key);
+
         if (value !== undefined) {
             this.#registry.unregister(value);
         }
-    }
-
-    get [Symbol.toStringTag](): string {
-        return "WeakValueMap";
     }
 
     constructor(input?: Iterable<[TKey, TValue]>) {
@@ -24,12 +21,16 @@ class WeakValueMap<TKey, TValue extends WeakValueMapValue> implements Map<TKey, 
         this.#registry = new FinalizationRegistry((key: TKey) => {
             this.#map.delete(key);
         });
-    
+
         if (input !== undefined && input !== null) {
             for (const [key, value] of input) {
                 this.set(key, value);
             }
         }
+    }
+
+    get [Symbol.toStringTag](): string {
+        return "WeakValueMap";
     }
 
     get size(): number {
@@ -38,6 +39,31 @@ class WeakValueMap<TKey, TValue extends WeakValueMapValue> implements Map<TKey, 
 
     get(key: TKey): TValue | undefined {
         return this.#map.get(key)?.deref()!;
+    }
+
+    getOrInsert(key: TKey, defaultValue: TValue): TValue {
+        const ref = this.#map.get(key);
+
+        if (ref === undefined) {
+          this.#registry.register(defaultValue, key, defaultValue);
+          this.#map.set(key, new WeakRef(defaultValue));
+          return defaultValue;
+        }
+
+        return ref.deref()!;
+    }
+
+    getOrInsertComputed(key: TKey, callback: (key: TKey) => TValue): TValue {
+      const ref = this.#map.get(key);
+
+        if (ref === undefined) {
+          const defaultValue = callback(key);
+          this.#registry.register(defaultValue, key, defaultValue);
+          this.#map.set(key, new WeakRef(defaultValue));
+          return defaultValue;
+        }
+
+        return ref.deref()!;
     }
 
     set(key: TKey, value: TValue): this {
@@ -57,38 +83,39 @@ class WeakValueMap<TKey, TValue extends WeakValueMapValue> implements Map<TKey, 
     }
 
     clear(): void {
-        for (const key of this.#map.keys()) {
-            this.#unregister(key);
-            this.#map.delete(key);
+        for (const value of this.#map.values()) {
+            this.#registry.unregister(value);
         }
+
+        this.#map.clear();
     }
 
     forEach(callbackfn: (value: TValue, key: TKey, map: Map<TKey, TValue>) => void, thisArg?: any): void {
         this.#map.forEach((value, key) => {
             callbackfn.call(thisArg, value.deref()!, key, this);
-        }, thisArg);
+        });
     }
 
-    *entries(): IterableIterator<[TKey, TValue]> {
+    *entries(): MapIterator<[TKey, TValue]> {
         for (const [key, value] of this.#map.entries()) {
             yield [key, value.deref()!];
         }
     }
 
-    *keys(): IterableIterator<TKey> {
+    *keys(): MapIterator<TKey> {
         for (const key of this.#map.keys()) {
             yield key;
         }
     }
 
-    *values(): IterableIterator<TValue> {
+    *values(): MapIterator<TValue> {
         for (const value of this.#map.values()) {
             yield value.deref()!;
         }
     }
 }
 
-interface WeakValueMap<TKey, TValue extends WeakValueMapValue> {
+interface WeakValueMap<TKey, TValue extends WeakRefTarget> {
     [Symbol.iterator]: typeof WeakValueMap.prototype.entries;
 }
 
